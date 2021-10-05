9685057c769303986aca2a3076eaa43c8c038426
WonDaeHo <77261907+WonDaeHo@users.noreply.github.com>
2021-10-05 12:04:53 +0900

    update

--- src/route/boards.js
+++ src/route/boards.js
@@ -64,37 +64,47 @@ boardRouter.get("/:id", (req, res) => {
     
 });
 
-boardRouter.post("", (req, res) => {
-    const createBoard = req.body;
-    const check_board = _.find(boards, ["id", createBoard.id]);
-    let result;
-    if(!check_board && createBoard.id && createBoard.title && createBoard.content && createBoard.createDate && createBoard.updateDate){
-        boards.push(createBoard);
-        result = `${createBoard.title} 게시글이 생성되었습니다.`
-    } else {
-        result = '입력 요청값이 잘못되었습니다.'
+boardRouter.post("/", async(req, res) => {
+    try{
+    const { content, title } = req.body;
+    if(!title) {
+    res.status(400).send({msg: "입력요청값이 잘못되었습니다."});
     }
-    res.status(201).send({
-        result
+    const result = await Board.create({
+    title: title ? title : null, 
+    content: content ? content : null
     });
-});
-
-boardRouter.put("/:id", (req, res) => {
-    const findBoard = _.find(boards, {id: parseInt(req.params.id)});
-    let result;
-    if(findBoard && findBoard.id == req.params.id){
-        findBoard.title = req.body.title;
-        result = `게시판 제목을 ${findBoard.title}으로 변경`;
-        res.status(200).send({
-            result
-        });
-    } else {
-        result = `아이디가 ${req.params.id}인 글이 존재하지 않습니다.`;
-        res.status(400).send({
-            result
-        });
+    
+    res.status(201).send({
+    msg: `id ${result.id}, ${result.title} 게시글이 생성되었습니다.`
+    });
+    }catch(err){
+    console.log(err);
+    res.status(500).send({msg: "서버에 문제가 발생했습니다. 잠시 후 다시 시도해주세요."})
     }
-});
+    });
+
+boardRouter.put("/:id", async(req, res) => {
+    try{
+    const { title, content } = req.body;
+    let board = await Board.findOne({
+    where:{
+    id: req.params.id
+    }
+    })
+    if(!board || (!title && !content)){
+    res.status(400).send({msg: '게시글이 존재하지 않거나 입력값이 잘못 되었습니다.'});
+    }
+    if(title) board.title = title;
+    if(content) board.content = content;
+    await board.save();
+    res.status(200).send({msg: '게시글이 정상적으로 수정 되었습니다.'})
+    
+    }catch(err){
+    console.log(err);
+    res.status(500).send({msg: "서버에 문제가 발생했습니다. 잠시 후 다시 시도해주세요."})
+    } 
+    });
 
 boardRouter.delete("/:id", async(req, res) => {
     try{
